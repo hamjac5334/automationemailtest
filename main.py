f4a31b27fde785213d04aa78e1fec194d36785a0
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2025-09-23 22:16:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from dsd_downloader import download_report
 from gmail_utils import gmail_authenticate, send_message_with_attachment
 from datetime import datetime
 import os
+import json
 
+with open('credentials.json', 'r') as f:
+    creds = json.load(f)
 
 USERNAME = os.environ["DSD_USERNAME"]
 PASSWORD = os.environ["DSD_PASSWORD"]
