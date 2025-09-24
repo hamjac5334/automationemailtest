78a09876379dc5d6355fed235b8a760e0259b145
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2025-09-24 01:01:21 -0500

    Update gmail_utils.py

--- gmail_utils.py
+++ gmail_utils.py
@@ -22,7 +22,7 @@ def gmail_authenticate():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_console()
+            creds = flow.run_local_server(port=0)
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
