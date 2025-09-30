2c713a3412dab47687909b729465f89083c5cc75
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2025-09-30 01:42:16 -0500

    Update main.py
    This is a test to run without gmail

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
 #init everything
 from dsd_downloader import download_report
-from gmail_utils import gmail_authenticate, send_message_with_attachment
-from datetime import datetime
-import os
-import json
+# from gmail_utils import gmail_authenticate, send_message
 
-with open('credentials.json', 'r') as f:
-    creds = json.load(f)
+import os
 
 USERNAME = os.environ["DSD_USERNAME"]
 PASSWORD = os.environ["DSD_PASSWORD"]
-SENDER_EMAIL = "jackson@bogmayer.com"
-RECEIVER_EMAIL = "jackson@bogmayer.com"
 
-if __name__ == "__main__":
-    print("Downloading report...")
-    report_path = download_report(USERNAME, PASSWORD)
-    print(f"Downloaded report to {report_path}")
+print("Downloading report...")
+report_path = download_report(USERNAME, PASSWORD)
+print(f"Downloaded report to {report_path}")
 
-    date_str = datetime.now().strftime("%Y-%m-%d")
-    subject = f"Automated Report - {date_str}"
-
-    print("Sending email...")
-    service = gmail_authenticate()
-    send_message_with_attachment(
-        service,
-        sender=SENDER_EMAIL,
-        to=RECEIVER_EMAIL,
-        subject=subject,
-        body_text="Attached is the latest test automated report. This is currently run locally. I am setting up a server to run it globally",
-        file=report_path,
-    )
-    print("Email sent!")
+# ---- TEMP DISABLED GMAIL ----
+# print("Sending email...")
+# service = gmail_authenticate()
+# send_message(service, "your_email@example.com", "Report", "Here is the report.", report_path)
+# print("Email sent!")
