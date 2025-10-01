0dd2d7125ce02473fa16e3f1b9c43e80c9e7c7e9
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2025-10-01 02:13:12 -0500

    Update gmail_utils.py
    makes this easier

--- gmail_utils.py
+++ gmail_utils.py
@@ -23,9 +23,9 @@ def gmail_authenticate():
 
 def send_email(service, sender, to, subject, attachment_path=None):
     message = MIMEMultipart()
-    message["to"] = "jackson@bogmayer.com"
-    message["from"] = "jackson@bogmayer.com"
-    message["subject"] = "This is an email test"
+    message["to"] = to
+    message["from"] = sender
+    message["subject"] = subject
     message.attach(MIMEText("Here is the test report attached from GitHub.", "plain"))
 
     if attachment_path:
