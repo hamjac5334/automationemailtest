a01fbd8893544c93f7eefd761aeec3b5c80609f7
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2025-10-01 01:37:01 -0500

    Update gmail_utils.py

--- gmail_utils.py
+++ gmail_utils.py
@@ -15,6 +15,31 @@ from email import encoders
 import base64
 import os
 
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+def gmail_authenticate():
+    SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+
+
+
+
 def send_email(service, sender, to, subject, attachment_path=None):
     # Create a multipart/mixed message (needed for attachments)
     message = MIMEMultipart("mixed")
@@ -39,5 +64,9 @@ def send_email(service, sender, to, subject, attachment_path=None):
 
     # Encode message and send
     raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
     service.users().messages().send(userId="me", body={"raw": raw_message}).execute()
+
+
+
 
