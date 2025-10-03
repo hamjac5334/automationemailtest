a743e57511c30aabe3406db3b58ea4e495d4f986
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2025-10-02 23:06:00 -0500

    Update main.py
    revert back

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 #init everything
-from dsd_downloader import download_reports
+#init everything
+from dsd_downloader import download_report
 from gmail_utils import gmail_authenticate, send_email
 import os
 
 USERNAME = os.environ["DSD_USERNAME"]
 PASSWORD = os.environ["DSD_PASSWORD"]
 
-print("Downloading reports...")
-
-report_links = [
-    ("https://dsdlink.com/Home?DashboardID=100120&ReportID=22835190", "InventoryReport"),
-    ("https://dsdlink.com/Home?DashboardID=100120&ReportID=22818254", "TestReport")
-]
-
-report_paths = download_reports(USERNAME, PASSWORD, report_links)
-print(f"Downloaded reports: {report_paths}")
+print("Downloading report...")
+report_path = download_report(USERNAME, PASSWORD)
+print(f"Downloaded report to {report_path}")
 
 print("Sending email...")
 service = gmail_authenticate()
+
+#mason.holland@hollandplace.net, chad.elkins@tapsandtables.net
+recipients = "jackson@bogmayer.com"
 send_email(
-    service,
-    sender=os.environ["GMAIL_ADDRESS"],
-    to=os.environ["GMAIL_RECIPIENT"],
-    subject="Daily Automated Reports",
-    body_text="Attached are today's reports.",
-    attachment_paths=report_paths
+    service=service,
+    sender="jackson@bogmayer.com",
+    to=recipients,
+    subject="Daily Report",
+    attachment_path=report_path
 )
 print("Email sent!")
