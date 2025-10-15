fb558cd43e0372ee2a0d8b118fbf0df2e10e9268
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2025-10-15 02:46:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-#mason.holland@hollandplace.net, chad.elkins@tapsandtables.net
-
 #init everything
-from dsd_downloader import download_reports
+from dsd_downloader import download_report
 from gmail_utils import gmail_authenticate, send_email
 import os
 
 USERNAME = os.environ["DSD_USERNAME"]
 PASSWORD = os.environ["DSD_PASSWORD"]
 
-print("Starting report downloads...")
+print("Downloading report...")
+report_path = download_report(USERNAME, PASSWORD)
+print(f"Downloaded report to {report_path}")
 
-# Define the reports to download
-reports = [
-    {"name": "Inventory_Report", "url": "https://dsdlink.com/Home?DashboardID=100120&ReportID=22835190"},
-    {"name": "Sales_Report", "url": "https://dsdlink.com/Home?DashboardID=100120&ReportID=22835190"},
-]
-
-# Download all reports in a single browser session
-downloaded_files = download_reports(USERNAME, PASSWORD, reports)
-
-print("All reports downloaded successfully.")
-
-# Authenticate Gmail
-print("Authenticating Gmail...")
+print("Sending email...")
 service = gmail_authenticate()
 
+#mason.holland@hollandplace.net, chad.elkins@tapsandtables.net
 recipients = "jackson@bogmayer.com"
 send_email(
     service=service,
     sender="jackson@bogmayer.com",
     to=recipients,
     subject="Daily Report",
-    attachment_paths=downloaded_files
+    attachment_path=report_path
 )
 print("Email sent!")
