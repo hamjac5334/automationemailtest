b76cad1174b3b4e9d48308308458828432f10e68
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2025-11-05 01:06:38 -0600

    Add additional Gmail recipients for notifications

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ USERNAME = os.environ.get("DSD_USERNAME")
 PASSWORD = os.environ.get("DSD_PASSWORD")
 GMAIL_ADDRESS = os.environ.get("GMAIL_ADDRESS")
 #,"mason.holland@hollandplace.net", "chad.elkins@tapsandtables.net"
-GMAIL_RECIPIENTS = ["jackson@bogmayer.com"]
+GMAIL_RECIPIENTS = ["jackson@bogmayer.com","mason.holland@hollandplace.net", "chad.elkins@tapsandtables.net"]
 
 REPORTS = [
     ("Sales Summary", "https://dsdlink.com/Home?DashboardID=100120&ReportID=22972383"),
