e90b9856a93938485293e2225044a0da731555d1
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2026-03-12 12:20:14 -0400

    Update GMAIL_RECIPIENTS with new email addresses
    Added additional email recipients to GMAIL_RECIPIENTS list.

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ USERNAME = os.environ.get("DSD_USERNAME")
 PASSWORD = os.environ.get("DSD_PASSWORD")
 GMAIL_ADDRESS = os.environ.get("GMAIL_ADDRESS")
 #, "mason.holland@hollandplace.net", "chad.elkins@tapsandtables.net", "michael.gallo@islandbrandsusa.com", "jared@bogmayer.com"
-GMAIL_RECIPIENTS = ["jackson@bogmayer.com" , "mason.holland@hollandplace.net", "chad.elkins@tapsandtables.net", "michael.gallo@islandbrandsusa.com", "jared@bogmayer.com"]
+#additional members: ,"carter@islandbrandsusa.com", "lauryn@rustybullbrewing.com", "max@southernbarrel.com", "ben@rustybullbrewing.com"
+GMAIL_RECIPIENTS = ["jackson@bogmayer.com" , "mason.holland@hollandplace.net", "chad.elkins@tapsandtables.net", "michael.gallo@islandbrandsusa.com", "jared@bogmayer.com","carter@islandbrandsusa.com", "lauryn@rustybullbrewing.com", "max@southernbarrel.com", "ben@rustybullbrewing.com"]
 
 REPORTS = [
     ("Sales Summary", "https://dsdlink.com/Home?DashboardID=100120&ReportID=22972383"),
