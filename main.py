28edbaac7b2f9342e9ca0e6e1991ae0b2d01e820
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2026-03-23 02:28:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,30 @@ GMAIL_ADDRESS = os.environ.get("GMAIL_ADDRESS")
 #GMAIL_RECIPIENTS = ["jackson@bogmayer.com" , "mason.holland@hollandplace.net", "chad.elkins@tapsandtables.net", "michael.gallo@islandbrandsusa.com", "jared@bogmayer.com", "ben@rustybullbrewing.com"]
 #GMAIL_RECIPIENTS = ["jackson@bogmayer.com"]
 
-MAIN_RECIPIENTS = ["jackson@bogmayer.com", "michael.gallo@islandbrandsusa.com"]
+MAIN_RECIPIENTS = [
+    "jackson@bogmayer.com", 
+    "michael.gallo@islandbrandsusa.com",
+    "mason.holland@hollandplace.net",
+    "chad.elkins@tapsandtables.net",
+    "jared@bogmayer.com",
+    "ben@rustybullbrewing.com" 
+]
 
 CHARLESTON_RECIPIENTS = [
     "jackson@bogmayer.com",
-    "michael.gallo@islandbrandsusa.com"
+    "michael.gallo@islandbrandsusa.com",
+    "carter@islandbrandsusa.com",
+    "lauryn@rustybullbrewing.com", 
+    "max@southernbarrel.com", 
+    "ben@rustybullbrewing.com"
+    
 ]
 
 GEORGIA_RECIPIENTS = [
     "jackson@bogmayer.com",
-    "michael.gallo@islandbrandsusa.com"
+    "michael.gallo@islandbrandsusa.com",
+    "max@southernbarrel.com", 
+    "ben@rustybullbrewing.com"
 ]
 
 
